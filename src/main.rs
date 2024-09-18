912a4e17e3e89ccbe7afed729ee13e4cc13ea7c3
Benjamin Warren <benwarren5020@gmail.com>
2024-09-18 13:21:40 +0200

    slices

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
 fn main() {
-    let s = String::from("hello");  // s comes into scope
 
-    takes_ownership(s);             // s's value moves into the function...
-                                    // ... and so is no longer valid here
-    
+  let s = String::from("hello world");
 
-    let x = 5;                      // x comes into scope
+    let word = first_word(&s);
 
-    makes_copy(x);                  // x would move into the function,
-                                    // but i32 is Copy, so it's okay to still
-                                    // use x afterward
+    // s.clear(); // error!
 
-} // Here, x goes out of scope, then s. But because s's value was moved, nothing
-  // special happens.
+    println!("the first word is: {word}");
+}
 
-fn takes_ownership(some_string: String) { // some_string comes into scope
-    println!("{some_string}");
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
+fn first_word(s: &String) -> &str {
+  let bytes = s.as_bytes();
 
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
-    println!("{some_integer}");
-} // Here, some_integer goes out of scope. Nothing special happens.
+  for (i, &item) in bytes.iter().enumerate() {
+      if item == b' ' {
+          return &s[0..i];
+      }
+  }
+
+  &s[..]
+}
