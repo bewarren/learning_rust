23b3a851998a8443223ee7005ea348bd19876a04
Benjamin Warren <benwarren5020@gmail.com>
2024-09-25 15:55:22 +0200

    associated functions

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,18 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
+    // fn area(&self) -> u32 {
+    //     self.width * self.height
+    // }
 
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
     }
 }
 
@@ -28,6 +34,11 @@ fn main() {
         height: 45,
     };
 
+    
+
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+
+    println!("Square value {:#?}", Rectangle::square(2))
 }
