e9be40322cc6d6d27746bdbc04c87efb9c86dcf3
Benjamin Warren <benwarren5020@gmail.com>
2024-10-02 11:13:52 +0200

    enum

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,26 @@
 #[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
 }
 
-impl Rectangle {
-    // fn area(&self) -> u32 {
-    //     self.width * self.height
-    // }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-    fn square(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size,
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
         }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
     }
 }
+
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
-
-    
-
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-
-
-    println!("Square value {:#?}", Rectangle::square(2))
+    let coin: Coin = Coin::Penny;
+    let penny = value_in_cents(coin);
+   println!("{:?}", penny);
 }
