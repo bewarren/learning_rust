6d3947120e7cf9c676cea07a5475ab85a00376da
Benjamin Warren <benwarren5020@gmail.com>
2024-10-02 11:16:22 +0200

    use inner value

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
-#[derive(Debug)]
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
+        Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
     }
 }
 
 
+
 fn main() {
-    let coin: Coin = Coin::Penny;
-    let penny = value_in_cents(coin);
-   println!("{:?}", penny);
+    let quarter = value_in_cents(Coin::Quarter(UsState::Alaska));
+   println!("{:?}", quarter);
 }
