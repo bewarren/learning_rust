17308688fde098f0b6fd6f2aaa52182acf913227
Benjamin Warren <benwarren5020@gmail.com>
2024-10-18 15:42:12 +0200

    vector iterate

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
-#[derive(Debug)] // so we can inspect the state in a minute
-enum UsState {
-    Alabama,
-    Alaska,
-    // --snip--
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {state:?}!");
-            25
-        }
-    }
-}
-
-
 
 fn main() {
-    let quarter = value_in_cents(Coin::Quarter(UsState::Alaska));
-   println!("{:?}", quarter);
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+
+    for i in &v {
+        println!("{i}");
+    }
+
 }
