6b968c14248c5d1dfbab171faf7af9bdabad2a72
Benjamin Warren <benwarren5020@gmail.com>
2024-10-25 10:48:48 +0200

    printing index

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 
-fn main() {
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50;
-    }
 
-    for i in &v {
-        println!("{i}");
-    }
+
+fn main() {
+    let hello = "Здравствуйте";
+
+    let s = &hello[0..4];
+
+    println!("{s}");
+
 
 }
