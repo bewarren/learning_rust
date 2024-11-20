be2983ccde0fce61b165683cb868310ad7ef4b8e
Benjamin Warren <benwarren5020@gmail.com>
2024-11-20 14:47:50 +0200

    panic

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,7 @@
 
 
 fn main() {
-    use std::collections::HashMap;
+    let v = vec![1, 2, 3];
 
-    let text = "hello world wonderful world";
-
-    let mut map = HashMap::new();
-
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
-    }
-
-    println!("{map:?}");
-
-
-
+    v[99];
 }
